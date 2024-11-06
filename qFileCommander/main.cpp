a45a865457885bb686f9053184d05b43b0744508
Млынник Семен <146517575+Mlynnik@users.noreply.github.com>
2024-11-06 23:02:42 +0300

    added disk functions
    added disk functions
    added helper funcctions for date and size transformation
    added saving settings to the registry
    
    Co-Authored-By: bluewater17 <146518438+bluewater17@users.noreply.github.com>

--- qFileCommander/main.cpp
+++ qFileCommander/main.cpp
@@ -4,10 +4,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setOrganizationName("qFileCommander");
-    QCoreApplication::setOrganizationDomain("qFileCommander.com");
-    QCoreApplication::setApplicationName("qFileCommander 1.0");
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
