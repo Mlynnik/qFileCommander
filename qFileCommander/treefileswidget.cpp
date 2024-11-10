469acd91266ad16c3cde76d6902b3d4b08f8d96a
Млынник Семен <146517575+Mlynnik@users.noreply.github.com>
2024-11-10 23:09:57 +0300

    worked with TreeFilesWidget
    new functions:
    *transformed the selection
    *added keyboard shortcuts
    *added drag-and-drop
    
    Co-Authored-By: Marty64 <146516955+martysama64@users.noreply.github.com>

--- qFileCommander/treefileswidget.cpp
+++ qFileCommander/treefileswidget.cpp
@@ -1,12 +1,31 @@
 #include "treefileswidget.h"
 #include "helperfunctions.h"
-#include <QMimeData>
+#include <QClipboard>
+#include <QDrag>
+#include <QDragMoveEvent>
+#include <QMenu>
+#include <QApplication>
 #include <QFileIconProvider>
 #include <QMimeDatabase>
 
-TreeFilesWidget::TreeFilesWidget(QWidget *parent) : QTreeWidget{parent} {}
+TreeFilesWidget::TreeFilesWidget(QWidget *parent) : QTreeWidget{parent}
+{
+    connect(this, SIGNAL(itemClicked(QTreeWidgetItem*,int)), this, SLOT(widget_itemClicked(QTreeWidgetItem*,int)));
+    connect(this, SIGNAL(currentItemChanged(QTreeWidgetItem*,QTreeWidgetItem*)), this, SLOT(current_itemChanged(QTreeWidgetItem*,QTreeWidgetItem*)));
+}
 
-//заполнение дерева по директории
+QList<QTreeWidgetItem *> TreeFilesWidget::selectedItems() const
+{
+    QList<QTreeWidgetItem *> res = *new QList<QTreeWidgetItem *>();
+    for (int i = 0; i < this->topLevelItemCount(); ++i) {
+        if (list_selected[i])
+            res.append(this->topLevelItem(i));
+    }
+    if ((res.length() < 1) && (this->topLevelItemCount() > 0))
+        res.append(this->currentItem());
+    return res;
+}
+
 void TreeFilesWidget::Fill(const QString &dir_str, bool hidden_file, QString last_dir_l)
 {
     QDir dir(dir_str);
@@ -167,3 +186,343 @@ void TreeFilesWidget::Fill(const QString &dir_str, bool hidden_file, QString las
         }
     }
 }
+
+
+void TreeFilesWidget::unselected_all()
+{
+    for (int i = 0; i < this->topLevelItemCount(); ++i) {
+        change_select(i, false);
+    }
+}
+
+void TreeFilesWidget::change_select(int ind, bool is_sel)
+{
+    if (is_sel) {
+        list_selected[ind] = true;
+        this->topLevelItem(ind)->setForeground(0, QColor(255, 0, 0));
+        this->topLevelItem(ind)->setForeground(1, QColor(255, 0, 0));
+        this->topLevelItem(ind)->setForeground(2, QColor(255, 0, 0));
+        this->topLevelItem(ind)->setForeground(3, QColor(255, 0, 0));
+    } else {
+        list_selected[ind] = false;
+        this->topLevelItem(ind)->setForeground(0, list_colors[ind]);
+        this->topLevelItem(ind)->setForeground(1, list_colors[ind]);
+        this->topLevelItem(ind)->setForeground(2, list_colors[ind]);
+        this->topLevelItem(ind)->setForeground(3, list_colors[ind]);
+    }
+}
+
+int TreeFilesWidget::count_selected()
+{
+    return list_selected.count(true);
+}
+
+
+void TreeFilesWidget::current_itemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous)
+{
+    previous_item_ind = this->indexOfTopLevelItem(previous);
+    if (QGuiApplication::keyboardModifiers() != Qt::ShiftModifier)
+        flag_shift = false;
+}
+
+void TreeFilesWidget::widget_itemClicked(QTreeWidgetItem *item, int column)
+{
+    int ind = this->indexOfTopLevelItem(item);
+    if (previous_item_ind == -1)
+        previous_item_ind = 0;
+
+    if (QGuiApplication::keyboardModifiers() == Qt::ControlModifier) {
+        if (list_selected[ind]) {
+            change_select(ind, false);
+        } else {
+            if (list_selected.indexOf(true) == -1) {
+                change_select(previous_item_ind, true);
+            }
+
+            change_select(ind, true);
+        }
+        flag_shift = false;
+    } else if (QGuiApplication::keyboardModifiers() == Qt::ShiftModifier) {
+
+        if (!(flag_shift && (list_selected.count(true) > 1))) {
+            begin_shift = previous_item_ind;
+        }
+
+        int f, l;
+        if (ind < begin_shift) {
+            f = ind;
+            l = begin_shift;
+        } else {
+            f = begin_shift;
+            l = ind;
+        }
+
+        for (int i = 0; i < f; ++i) {
+            change_select(i, false);
+        }
+
+        for (int i = f; i <= l; ++i) {
+            change_select(i, true);
+        }
+
+        for (int i = l + 1; i < this->topLevelItemCount(); ++i) {
+            change_select(i, false);
+        }
+
+        flag_shift = true;
+    } else {
+        flag_shift = false;
+        if (count_selected() > 0)
+            this->unselected_all();
+    }
+    previous_item_ind = this->indexOfTopLevelItem(item);
+    emit this->itemSelectionChanged();
+}
+
+
+void TreeFilesWidget::keyPressEvent(QKeyEvent *event)
+{
+    if (indexOfTopLevelItem(currentItem()) == -1) {
+        setCurrentItem(topLevelItem(0));
+        previous_item_ind = 0;
+    }
+    if (event->modifiers() == Qt::ShiftModifier) {
+        if (event->key() == Qt::Key_Up) {
+            if (!flag_shift) {
+                unselected_all();
+                begin_shift = previous_item_ind;
+                flag_shift = true;
+
+                change_select(begin_shift, true);
+            }
+
+            if (this->currentIndex().row() > 0) {
+                int n_ind = this->indexOfTopLevelItem(currentItem());
+                if (list_selected[n_ind]) {
+                    if (list_selected[n_ind - 1]) {
+                        change_select(n_ind, false);
+                    } else {
+                        --n_ind;
+                        change_select(n_ind, true);
+                    }
+                } else {
+                    --n_ind;
+                    change_select(n_ind, true);
+                }
+                this->setCurrentItem(this->topLevelItem(this->currentIndex().row() - 1));
+            }
+            return;
+        } else if (event->key() == Qt::Key_Down) {
+            if (!flag_shift) {
+                unselected_all();
+                begin_shift = previous_item_ind;
+                flag_shift = true;
+
+                change_select(begin_shift, true);
+            }
+
+            if (this->currentIndex().row() + 1 < this->topLevelItemCount()) {
+                int n_ind = this->indexOfTopLevelItem(currentItem());
+                if (list_selected[n_ind]) {
+                    if (list_selected[n_ind + 1]) {
+                        change_select(n_ind, false);
+                    } else {
+                        ++n_ind;
+                        change_select(n_ind, true);
+                    }
+                } else {
+                    ++n_ind;
+                    change_select(n_ind, true);
+                }
+                this->setCurrentItem(this->topLevelItem(this->currentIndex().row() + 1));
+            }
+            return;
+        }
+    } else if (flag_shift)
+        flag_shift = false;
+
+    if (event->modifiers() == Qt::ControlModifier) {
+        if (event->key() == Qt::Key_A) {
+            if (!((this->topLevelItem(0)->text(0) == "..") && (this->topLevelItem(0)->text(1) == "<DIR>"))) {
+                change_select(0, true);
+            }
+
+            for (int i = 1; i < this->topLevelItemCount(); ++i) {
+                change_select(i, true);
+            }
+            return;
+        } else if ((event->key() == Qt::Key_C) || (event->key() == Qt::Key_X)) {
+            auto mimeData = new QMimeData;
+
+            const QList<QTreeWidgetItem *> &items = this->selectedItems();
+            QList<QUrl> urls;
+            for (int i = 0; i < items.length(); ++i) {
+                if ((items[i]->text(0) == "..") && (items[i]->text(1) == "<DIR>"))
+                    continue;
+                QString suffics = "";
+                if (items[i]->text(1) != "<DIR>")
+                    suffics = "." % items[i]->text(1);
+                urls << QUrl::fromLocalFile(path % items[i]->text(0) % suffics);
+            }
+            mimeData->setUrls(urls);
+
+            int dropEffect;
+            if (event->key() == Qt::Key_C)
+                dropEffect = 5;
+            else
+                dropEffect = 2;
+
+            QByteArray data;
+            QDataStream stream(&data, QIODevice::WriteOnly);
+            stream.setByteOrder(QDataStream::LittleEndian);
+            stream << dropEffect;
+            mimeData->setData("Preferred DropEffect", data);
+
+            QApplication::clipboard()->setMimeData(mimeData);
+            return;
+
+        } else if (event->key() == Qt::Key_V) {
+            const QMimeData *mimeData = QApplication::clipboard()->mimeData();
+            if (mimeData->hasUrls()) {
+                QByteArray output_data = mimeData->data("Preferred DropEffect");
+
+                int dropEffect = 2;
+
+                QByteArray data;
+                QDataStream stream(&data, QIODevice::WriteOnly);
+                stream.setByteOrder(QDataStream::LittleEndian);
+                stream << dropEffect;
+
+                QStringList pathList;
+                QList<QUrl> urlList = mimeData->urls();
+
+                for (int i = 0; i < urlList.size();++i)
+                {
+                    pathList.append(urlList.at(i).toLocalFile());
+                }
+                drop_func_signal(pathList, (data == output_data));
+                return;
+            }
+        }
+    } else if (event->key() == Qt::Key_Insert) {
+        int ind = this->currentIndex().row();
+        if (list_selected[ind]) {
+            change_select(ind, false);
+        } else {
+            change_select(ind, true);
+        }
+        if (this->currentIndex().row() + 1 < this->topLevelItemCount())
+            this->setCurrentItem(this->topLevelItem(this->currentIndex().row() + 1));
+        emit this->itemSelectionChanged();
+        return;
+    } else if (event->key() == Qt::Key_Space) {
+        int ind = this->currentIndex().row();
+        if (list_selected[ind]) {
+            change_select(ind, false);
+        } else {
+            change_select(ind, true);
+        }
+        emit this->itemSelectionChanged();
+        return;
+    }
+    QTreeWidget::keyPressEvent(event);
+}
+
+
+QStringList TreeFilesWidget::mimeTypes() const
+{
+    return QStringList(QLatin1String("text/uri-list"));
+}
+
+QMimeData* TreeFilesWidget::mimeData(const QList<QTreeWidgetItem *> &items) const
+{
+    QList<QUrl> urls;
+    for (int i = 0; i < items.length(); ++i) {
+        if ((items[i]->text(0) == "..") && (items[i]->text(1) == "<DIR>"))
+            continue;
+        QString suffics = "";
+        if (items[i]->text(1) != "<DIR>")
+            suffics = "." % items[i]->text(1);
+        urls << QUrl::fromLocalFile(path % items[i]->text(0) % suffics);
+    }
+    QMimeData* data = new QMimeData();
+    data->setUrls(urls);
+    return data;
+}
+
+void TreeFilesWidget::startDrag(Qt::DropActions supportedActions)
+{
+    if(supportedActions){
+        QList<QTreeWidgetItem *> m_items = selectedItems();
+        if(m_items.isEmpty())
+            return;
+        QMimeData *data = mimeData(m_items);
+        QDrag *drag = new QDrag(this);
+        QPixmap pixmap = QIcon(style()->standardIcon(QStyle::SP_FileIcon)).pixmap(30, 50);
+        drag->setPixmap(pixmap);
+        drag->setMimeData(data);
+        drag->setHotSpot(pixmap.rect().center());
+        drag->exec();
+    }
+    else
+        QTreeWidget::startDrag(supportedActions);
+}
+
+void TreeFilesWidget::dragEnterEvent(QDragEnterEvent* event)
+{
+    if(event->buttons() == Qt::LeftButton) {
+        is_r_move = false;
+    } else {
+        is_r_move = true;
+    }
+    event->acceptProposedAction();
+}
+
+void TreeFilesWidget::dragMoveEvent(QDragMoveEvent* event)
+{
+    event->acceptProposedAction();
+}
+
+void TreeFilesWidget::dragLeaveEvent(QDragLeaveEvent* event)
+{
+    event->accept();
+}
+
+void TreeFilesWidget::dropEvent(QDropEvent* event)
+{
+    const QMimeData* mimeData = event->mimeData();
+
+    if (mimeData->hasUrls())
+    {
+        QStringList pathList;
+        QList<QUrl> urlList = mimeData->urls();
+
+        for (int i = 0; i < urlList.size();++i)
+        {
+            pathList.append(urlList.at(i).toLocalFile());
+        }
+
+        if (QFileInfo(pathList.first()).absoluteDir() != QDir(path)) {
+            if (is_r_move) {
+                QMenu *menu = new QMenu(this);
+                QAction *copy_action = new QAction("Копировать", this);
+                QAction *move_action = new QAction("Переместить", this);
+                connect(copy_action, &QAction::triggered, this, [this, pathList] { drop_func_signal(pathList, false); });
+                connect(move_action, &QAction::triggered, this, [this, pathList] { drop_func_signal(pathList, true); });
+                menu->addAction(copy_action);
+                menu->addAction(move_action);
+                menu->popup(this->viewport()->mapToGlobal(event->position().toPoint()));
+            } else {
+                drop_func_signal(pathList, true);
+            }
+        }
+        event->acceptProposedAction();
+    }
+}
+
+void TreeFilesWidget::drop_func_signal(QStringList lst, bool remove_after)
+{
+    emit drop_signal(lst, remove_after);
+    //TODO signal to mainwindow
+}
+
--- qFileCommander/treefileswidget.h
+++ qFileCommander/treefileswidget.h
@@ -10,13 +10,6 @@
 class TreeFilesWidget : public QTreeWidget
 {
     Q_OBJECT
-private:
-    ///список с цветами файлов
-    QList<QColor> list_colors = *new QList<QColor>();
-    ///список выделенных item
-    QList<bool> list_selected = *new QList<bool>();
-
-
 public:
     explicit TreeFilesWidget(QWidget *parent = nullptr);
     ///активная директория
@@ -28,9 +21,56 @@ public:
     ///кол-во файлов в активной директории
     int all_f = 0;
 
-public slots:
-    void Fill(const QString &dir_str, bool hidden_file, QString last_dir_l);
+    ///выделенные item
+    QList<QTreeWidgetItem *> selectedItems() const;
 
+signals:
+    void drop_signal(QStringList lst, bool remove_after);
+
+public slots:
+    ///заполнение дерева по директории
+    void Fill(const QString &dir_str, bool hidden_file, QString last_dir_l);
+    ///кол-во выделенных item
+    int count_selected();
+
+private:
+    ///true - move file, false - copy
+    bool is_r_move = true;
+    ///индекс предыдущего (по времени выделения) item
+    int previous_item_ind;
+    ///является ли shift-выделением
+    bool flag_shift = false;
+    ///индекс, который был перед (shift + clickItem)
+    int begin_shift;
+
+    QStringList mimeTypes() const override;
+    QMimeData* mimeData(const QList<QTreeWidgetItem *> &items) const override;
+    void startDrag(Qt::DropActions supportedActions) override;
+
+
+    ///список с цветами файлов
+    QList<QColor> list_colors = *new QList<QColor>();
+    ///список выделенных item
+    QList<bool> list_selected = *new QList<bool>();
+
+private slots:
+    ///смена активного item
+    void current_itemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
+    ///клик по item
+    void widget_itemClicked(QTreeWidgetItem *item, int column);
+    ///снять выделение
+    void unselected_all();
+    ///второй режим выделения
+    void change_select(int ind, bool is_sel);
+
+    void keyPressEvent(QKeyEvent *event) override;
+
+    void drop_func_signal(QStringList lst, bool remove_after);
+
+    void dragEnterEvent(QDragEnterEvent* event) override;
+    void dragMoveEvent(QDragMoveEvent* event) override;
+    void dragLeaveEvent(QDragLeaveEvent* event) override;
+    void dropEvent(QDropEvent* event) override;
 };
 
 #endif // TREEFILESWIDGET_H
